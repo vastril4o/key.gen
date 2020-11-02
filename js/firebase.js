729c85659ae4589d42b6772dfec52c7461a34f4f
vasil <vastril4o@gmail.com>
2020-11-03 01:50:43 +0200

    firebase > typo in js fix

--- js/firebase.js
+++ js/firebase.js
@@ -27,8 +27,8 @@ counterDislike.on("value", (snapshot) => {
 // update
 function fbCounterIncrease(type) {
     document.getElementById("pFeedback").innerHTML = "Thanks for your feedback!";
-    document.getElementById("aLike").style.display = "none"
-    document.getElementById("aDislike").style.display = "none"
+    document.getElementById("aLike").style.display = "none";
+    document.getElementById("aDislike").style.display = "none";
     if (type == 'like') {
         counterLike.transaction(val => val + 1, (err) => {
             if (err) {
